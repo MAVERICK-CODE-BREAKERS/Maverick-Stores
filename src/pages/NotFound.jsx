0f72f3edfe11475b9a7b2b97471f40f04a0a812c
Abdulrahman Afaraetu <afaraeturahman@gmail.com>
2021-11-04 17:36:32 +0100

    nav

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import error from './../assets/error.jpeg'
+// import error from './../assets/error.jpeg'
 import { Link } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 
