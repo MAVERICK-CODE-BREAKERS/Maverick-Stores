986d7788c56af0148ba59a08344e2e947b6f75f1
unknown <salihu45@gmail.com>
2021-11-11 08:27:59 +0100

    continuation of product category

--- src/components/Navbar/Header.jsx
+++ src/components/Navbar/Header.jsx
@@ -8,61 +8,72 @@ const Header = () => {
   const [cart, setCart] = useState(false)
 
   return (
-    <nav className='navbar'>
-      <Link to='/' className='logo mb-2'>
+    <nav className="navbar">
+      <Link to="/" className="logo mb-2">
         <img
           src={image}
           height={90}
           width={150}
-          alt='Maverick Digital Stores'
+          alt="Maverick Digital Stores"
         />
       </Link>
       <ul
-        className={menuBar ? 'nav-links-mobile' : 'nav-links'}
+        className={menuBar ? "nav-links-mobile" : "nav-links"}
         onClick={() => setMenuBar(false)}
       >
-        <Link to='/products/console' className='category'>
+        <Link
+          to="/Product-Category"
+          className="category"
+        >
           <li>Consoles</li>
         </Link>
-        <Link to='/products/laptops' className='category'>
+        <Link
+          to="/Product-Category"
+          className="category"
+        >
           <li>Laptops</li>
         </Link>
-        <Link to='/products/watches' className='category'>
+        <Link
+          to="/Product-Category"
+          className="category"
+        >
           <li>Watches</li>
         </Link>
-        <Link to='/products/accessories' className='category me-5'>
+        <Link
+          to="/Product-Category"
+        >
           <li> Accessories</li>
         </Link>
 
-        <Link to='/cart' className='category me-3 ca'>
+        <Link to="/cart" className="category me-3 ca">
           <li>
-            <i className='fas fa-shopping-cart' /> Cart
+            <i className="fas fa-shopping-cart" /> Cart
           </li>
         </Link>
 
-        <Link to='/login' className='signup ms-3'>
+        <Link to="/login" className="signup ms-3">
           <li>Login/Register</li>
         </Link>
       </ul>
       <Link
-        to='/cart'
-        className={cart ? 'cart' : 'cart-mobile'}
+        to="/cart"
+        className={cart ? "cart" : "cart-mobile"}
         onClick={() => {
-          setMenuBar(false)
-          setCart(false)
+          setMenuBar(false);
+          setCart(false);
         }}
       >
-        <i className='fas fa-shopping-cart' />
+        <i className="fas fa-shopping-cart" />
       </Link>
-      <button className='mobile-menu-icon' onClick={() => setMenuBar(!menuBar)}>
+      <button className="mobile-menu-icon" onClick={() => setMenuBar(!menuBar)}>
         {menuBar ? (
-          <i className='fas fa-times'></i>
+          <i className="fas fa-times"></i>
         ) : (
-          <i className='fas fa-bars'></i>
+          <i className="fas fa-bars"></i>
         )}
       </button>
     </nav>
-  )
+  );
 }
 
 export default Header
