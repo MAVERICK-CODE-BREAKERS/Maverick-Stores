df2ccec9f5d45a5fdca07639a072a806ffd063a4
Abdulrahman Afaraetu <afaraeturahman@gmail.com>
2021-11-14 18:35:34 +0100

    product cat

--- src/pages/Product-Category/ProductCategory.jsx
+++ src/pages/Product-Category/ProductCategory.jsx
@@ -1,167 +1,168 @@
-import React, { useState } from "react";
-import { Col, Row } from "react-bootstrap";
-import Nav from "react-bootstrap/Nav";
-import Form from "react-bootstrap/Form";
-import products from "../../products";
-import Product from "../../components/Product";
-import product from "../ProductScreen";
-import Image from "react-bootstrap/Image";
-import Banner from "../../assets/slides/Banner.jpg";
-import StyledProductCategory from "./ProductCategory.Module.css";
+import React, { useState } from 'react'
+import { Col, Row } from 'react-bootstrap'
+import Nav from 'react-bootstrap/Nav'
+import Form from 'react-bootstrap/Form'
+import products from '../../products'
+import Product from '../../components/Product'
+// import product from '../ProductScreen'
+import Image from 'react-bootstrap/Image'
+import Banner from '../../assets/slides/Banner.jpg'
+import StyledProductCategory from './ProductCategory.Module.css'
 
 const ProductCategory = ({ history, match }) => {
+  // const product = products.find((p) => p._id === match.params.id)
   // products checkbox states
-  const [isShowLaptopForm, setIsShowLaptopForm] = useState(true);
-  const [isShowConsolesForm, setIsShowConsolesForm] = useState(false);
-  const [isShowAccessoriesForm, setIsShowAccessoriesForm] = useState(false);
-  const [isShowSmartWatchesForm, setIsShowSmartWatchesForm] = useState(false);
+  const [isShowLaptopForm, setIsShowLaptopForm] = useState(true)
+  const [isShowConsolesForm, setIsShowConsolesForm] = useState(false)
+  const [isShowAccessoriesForm, setIsShowAccessoriesForm] = useState(false)
+  const [isShowSmartWatchesForm, setIsShowSmartWatchesForm] = useState(false)
 
   // product container states
   const [isShowLaptopProductContainer, setIsShowLaptopProductContainer] =
-    useState(true);
-  const [isShowConsolesContainer, setIShowConsolesContainer] = useState(false);
+    useState(true)
+  const [isShowConsolesContainer, setIShowConsolesContainer] = useState(false)
   const [isShowAccessoriesContainer, setIsShowAccessoriesContainer] =
-    useState(false);
+    useState(false)
   const [isShowSmartWatchesContainer, setIsShowSmartWatchesContainer] =
-    useState(false);
+    useState(false)
 
   const handleClickShow = () => {
-    setIsShowConsolesForm(true);
-    setIShowConsolesContainer(true);
-    setIsShowAccessoriesForm(false);
-    setIsShowLaptopForm(false);
-    setIsShowSmartWatchesForm(false);
-    setIsShowLaptopProductContainer(false);
-    setIsShowSmartWatchesContainer(false);
-    setIsShowAccessoriesContainer(false);
-  };
+    setIsShowConsolesForm(true)
+    setIShowConsolesContainer(true)
+    setIsShowAccessoriesForm(false)
+    setIsShowLaptopForm(false)
+    setIsShowSmartWatchesForm(false)
+    setIsShowLaptopProductContainer(false)
+    setIsShowSmartWatchesContainer(false)
+    setIsShowAccessoriesContainer(false)
+  }
   const handleClickShow1 = () => {
-    setIsShowAccessoriesForm(true);
-    setIsShowAccessoriesContainer(true);
-    setIsShowConsolesForm(false);
-    setIsShowLaptopForm(false);
-    setIsShowSmartWatchesForm(false);
-    setIsShowLaptopProductContainer(false);
-    setIShowConsolesContainer(false);
-    setIsShowSmartWatchesContainer(false);
-  };
+    setIsShowAccessoriesForm(true)
+    setIsShowAccessoriesContainer(true)
+    setIsShowConsolesForm(false)
+    setIsShowLaptopForm(false)
+    setIsShowSmartWatchesForm(false)
+    setIsShowLaptopProductContainer(false)
+    setIShowConsolesContainer(false)
+    setIsShowSmartWatchesContainer(false)
+  }
 
   const handleClickShow2 = () => {
-    setIsShowSmartWatchesForm(true);
-    setIsShowSmartWatchesContainer(true);
-    setIsShowAccessoriesForm(false);
-    setIsShowLaptopForm(false);
-    setIsShowConsolesForm(false);
-    setIsShowAccessoriesContainer(false);
-    setIsShowLaptopProductContainer(false);
-    setIShowConsolesContainer(false);
-  };
+    setIsShowSmartWatchesForm(true)
+    setIsShowSmartWatchesContainer(true)
+    setIsShowAccessoriesForm(false)
+    setIsShowLaptopForm(false)
+    setIsShowConsolesForm(false)
+    setIsShowAccessoriesContainer(false)
+    setIsShowLaptopProductContainer(false)
+    setIShowConsolesContainer(false)
+  }
 
   const handleClickShow3 = () => {
-    setIsShowSmartWatchesForm(false);
-    setIsShowAccessoriesForm(false);
-    setIsShowLaptopForm(true);
-    setIsShowConsolesForm(false);
-    setIsShowLaptopProductContainer(false);
-    setIsShowSmartWatchesContainer(false);
-    setIsShowAccessoriesContainer(false);
-    setIsShowLaptopProductContainer(true);
-  };
+    setIsShowSmartWatchesForm(false)
+    setIsShowAccessoriesForm(false)
+    setIsShowLaptopForm(true)
+    setIsShowConsolesForm(false)
+    setIsShowLaptopProductContainer(false)
+    setIsShowSmartWatchesContainer(false)
+    setIsShowAccessoriesContainer(false)
+    setIsShowLaptopProductContainer(true)
+  }
 
   return (
     <div>
-      <div className="image-container my-5">
+      <div className='image-container my-5'>
         <Image src={Banner} thumbnail />
       </div>
       <div className={StyledProductCategory.productContainer}>
         <Row>
-          <Col className="Smaller" xs={2}>
+          <Col className='Smaller' xs={2}>
             <div
               className={StyledProductCategory.laptopCheckboxContainer}
-              style={{ display: isShowLaptopForm ? "block" : "none" }}
+              style={{ display: isShowLaptopForm ? 'block' : 'none' }}
             >
               <Form>
-                <Form.Group className="mb-3" id="formGridCheckbox">
-                  <Form.Check type="checkbox" label="Apple" />
+                <Form.Group className='mb-3' id='formGridCheckbox'>
+                  <Form.Check type='checkbox' label='Apple' />
                 </Form.Group>
-                <Form.Group className="mb-3" id="formGridCheckbox">
-                  <Form.Check type="checkbox" label="Samsung" />
+                <Form.Group className='mb-3' id='formGridCheckbox'>
+                  <Form.Check type='checkbox' label='Samsung' />
                 </Form.Group>
-                <Form.Group className="mb-3" id="formGridCheckbox">
-                  <Form.Check type="checkbox" label="Huawei" />
+                <Form.Group className='mb-3' id='formGridCheckbox'>
+                  <Form.Check type='checkbox' label='Huawei' />
                 </Form.Group>
               </Form>
             </div>
             <div
               className={StyledProductCategory.consolesCheckboxContainer}
-              style={{ display: isShowConsolesForm ? "block" : "none" }}
+              style={{ display: isShowConsolesForm ? 'block' : 'none' }}
             >
               <Form>
-                <Form.Group className="mb-3" id="formGridCheckbox">
-                  <Form.Check type="checkbox" label="Playstation" />
+                <Form.Group className='mb-3' id='formGridCheckbox'>
+                  <Form.Check type='checkbox' label='Playstation' />
                 </Form.Group>
-                <Form.Group className="mb-3" id="formGridCheckbox">
-                  <Form.Check type="checkbox" label="Xbox" />
+                <Form.Group className='mb-3' id='formGridCheckbox'>
+                  <Form.Check type='checkbox' label='Xbox' />
                 </Form.Group>
-                <Form.Group className="mb-3" id="formGridCheckbox">
-                  <Form.Check type="checkbox" label="Nintendo" />
+                <Form.Group className='mb-3' id='formGridCheckbox'>
+                  <Form.Check type='checkbox' label='Nintendo' />
                 </Form.Group>
               </Form>
             </div>
             <div
               className={StyledProductCategory.accessoriesCheckboxContainer}
-              style={{ display: isShowAccessoriesForm ? "block" : "none" }}
+              style={{ display: isShowAccessoriesForm ? 'block' : 'none' }}
             >
               <Form>
-                <Form.Group className="mb-3" id="formGridCheckbox">
-                  <Form.Check type="checkbox" label="Speakers" />
+                <Form.Group className='mb-3' id='formGridCheckbox'>
+                  <Form.Check type='checkbox' label='Speakers' />
                 </Form.Group>
-                <Form.Group className="mb-3" id="formGridCheckbox">
-                  <Form.Check type="checkbox" label="Microphone" />
+                <Form.Group className='mb-3' id='formGridCheckbox'>
+                  <Form.Check type='checkbox' label='Microphone' />
                 </Form.Group>
-                <Form.Group className="mb-3" id="formGridCheckbox">
-                  <Form.Check type="checkbox" label="Power-Banks" />
+                <Form.Group className='mb-3' id='formGridCheckbox'>
+                  <Form.Check type='checkbox' label='Power-Banks' />
                 </Form.Group>
               </Form>
             </div>
             <div
               className={StyledProductCategory.SmartWatchesCheckboxContainer}
-              style={{ display: isShowSmartWatchesForm ? "block" : "none" }}
+              style={{ display: isShowSmartWatchesForm ? 'block' : 'none' }}
             >
               <Form>
-                <Form.Group className="mb-3" id="formGridCheckbox">
-                  <Form.Check type="checkbox" label="Apple" />
+                <Form.Group className='mb-3' id='formGridCheckbox'>
+                  <Form.Check type='checkbox' label='Apple' />
                 </Form.Group>
-                <Form.Group className="mb-3" id="formGridCheckbox">
-                  <Form.Check type="checkbox" label="Samsung" />
+                <Form.Group className='mb-3' id='formGridCheckbox'>
+                  <Form.Check type='checkbox' label='Samsung' />
                 </Form.Group>
-                <Form.Group className="mb-3" id="formGridCheckbox">
-                  <Form.Check type="checkbox" label="Xiaomi" />
+                <Form.Group className='mb-3' id='formGridCheckbox'>
+                  <Form.Check type='checkbox' label='Xiaomi' />
                 </Form.Group>
               </Form>
             </div>
             <div className={StyledProductCategory.PriceAdjustContainer}>
               <Form.Check
-                type="radio"
-                label="Lowest To Highest"
+                type='radio'
+                label='Lowest To Highest'
                 className={StyledProductCategory.priceLabel}
-                name="formHorizontalRadios"
-                id="formHorizontalRadios1"
+                name='formHorizontalRadios'
+                id='formHorizontalRadios1'
               />
               <Form.Check
-                type="radio"
-                label="Highest To Lowest"
+                type='radio'
+                label='Highest To Lowest'
                 className={StyledProductCategory.priceLabel}
-                name="formHorizontalRadios"
-                id="formHorizontalRadios2"
+                name='formHorizontalRadios'
+                id='formHorizontalRadios2'
               />
             </div>
           </Col>
-          <Col className="bigger" xs={10}>
-            <Nav fill variant="pills" defaultActiveKey="link">
+          <Col className='bigger' xs={10}>
+            <Nav fill variant='pills' defaultActiveKey='link'>
               <Nav.Item>
                 <Nav.Link
-                  eventKey="link"
+                  eventKey='link'
                   className={StyledProductCategory.navLink}
                   onClick={handleClickShow3}
                 >
@@ -170,7 +171,7 @@ const ProductCategory = ({ history, match }) => {
               </Nav.Item>
               <Nav.Item>
                 <Nav.Link
-                  eventKey="link-1"
+                  eventKey='link-1'
                   onClick={handleClickShow}
                   className={StyledProductCategory.navLink}
                 >
@@ -179,7 +180,7 @@ const ProductCategory = ({ history, match }) => {
               </Nav.Item>
               <Nav.Item>
                 <Nav.Link
-                  eventKey="link-2"
+                  eventKey='link-2'
                   onClick={handleClickShow1}
                   className={StyledProductCategory.navLink}
                 >
@@ -188,7 +189,7 @@ const ProductCategory = ({ history, match }) => {
               </Nav.Item>
               <Nav.Item>
                 <Nav.Link
-                  eventKey="link-3"
+                  eventKey='link-3'
                   onClick={handleClickShow2}
                   className={StyledProductCategory.navLink}
                 >
@@ -199,51 +200,42 @@ const ProductCategory = ({ history, match }) => {
             <div
               className={StyledProductCategory.laptopsProductsContainer}
               style={{
-                display: isShowLaptopProductContainer ? "block" : "none",
+                display: isShowLaptopProductContainer ? 'block' : 'none',
               }}
             >
-              {/* <Row>
+              <Row>
                 {products
-                  .filter((rel) => rel.category === product.category)
-                  .map(
-                    (relatedProducts) =>
-                      product._id !== relatedProducts._id && (
-                        <Col
-                          key={relatedProducts._id}
-                          sm={12}
-                          md={6}
-                          lg={4}
-                          xl={3}
-                        >
-                          <Product product={relatedProducts} size />
-                        </Col>
-                      )
-                  )}
-              </Row> */}
+                  .filter((rel) => rel.category === 'Laptop')
+                  .map((relatedProducts) => (
+                    <Col key={relatedProducts._id} sm={12} md={6} lg={4} xl={3}>
+                      <Product product={relatedProducts} size />
+                    </Col>
+                  ))}
+              </Row>
             </div>
             <div
               className={StyledProductCategory.consoleProductsContainer}
               style={{
-                display: isShowConsolesContainer ? "block" : "none",
+                display: isShowConsolesContainer ? 'block' : 'none',
               }}
             ></div>
             <div
               className={StyledProductCategory.accessoriesProductsContainer}
               style={{
-                display: isShowAccessoriesContainer ? "block" : "none",
+                display: isShowAccessoriesContainer ? 'block' : 'none',
               }}
             ></div>
             <div
               className={StyledProductCategory.smartWatchesProductsContainer}
               style={{
-                display: isShowSmartWatchesContainer ? "block" : "none",
+                display: isShowSmartWatchesContainer ? 'block' : 'none',
               }}
             ></div>
           </Col>
         </Row>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ProductCategory;
+export default ProductCategory
